17ce59abbc00463503279bc271d46dfa0a88f882
parkavi48 <parkavi816@gmail.com>
2025-12-10 12:32:51 +0530

    Add files via upload

---
+++ CheckNumberIsPositive.java
@@ -0,0 +1,13 @@
+package project1;
+
+public class CheckNumberIsPositive {
+	public static void main(String[] args) {
+		int a=10;
+		if (a>0)
+			System.out.println("Negative");
+		else
+			System.out.println("Positive");
+	}
+
+}
+ 
---
+++ Firefox.java
@@ -0,0 +1,19 @@
+package project1;
+
+public class Firefox {
+    public static void main(String[] args) {
+    	float browserVersion = 100.2f ;
+    	String browserName = "firefox" ;
+    	boolean isVisible = true ;
+    	int releaseYear = 1998 ;
+    	char browserLogo = 'f' ;
+    	System.out.println("Browser Version:"+browserVersion);
+    	System.out.println("Browser Name:"+browserName);
+    	System.out.println("Is Visible:"+isVisible);
+    	System.out.println("Browser Year:"+releaseYear);
+    	System.out.println("Browser logo:"+browserLogo);
+    	
+ 
+    }
+
+}
