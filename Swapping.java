801b4aafdb59c4e725bd252230afc2aceb4c60c4
parkavi48 <parkavi816@gmail.com>
2025-12-30 21:39:58 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		        int a = 10;
+		        int b = 20;
+
+		        // Swapping without using a third variable
+		        a = a + b;
+		        b = a - b;
+		        a = a - b;
+
+		        System.out.println("After swapping:");
+		        System.out.println("a = " + a);
+		        System.out.println("b = " + b);
+		    
+		}
+
+
+	}
+
+
